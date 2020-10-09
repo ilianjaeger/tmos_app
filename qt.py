9d34d53a7f01626bb1ffaeab04625031c3baa8bb
Tiago Salzmann <salzmann.tiago@gmail.com>
2020-10-09 10:56:05 +0200

    [ADD] First Qt Template

---
+++ qt.py
@@ -0,0 +1,52 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+import logging
+
+# Uncomment below for terminal log messages
+# logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(name)s - %(levelname)s - %(message)s')    
+
+class QPlainTextEditLogger(logging.Handler):
+    def __init__(self, parent):
+        super().__init__()
+        self.widget = QtWidgets.QPlainTextEdit(parent)
+        self.widget.setReadOnly(True)    
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.widget.appendPlainText(msg)    
+
+
+class MyDialog(QtWidgets.QWidget):
+    def __init__(self, *args):
+        QtWidgets.QWidget.__init__(self, *args)
+
+        logTextBox = QPlainTextEditLogger(self)
+        # You can format what is printed to text box
+        logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        logging.getLogger().addHandler(logTextBox)
+        # You can control the logging level
+        logging.getLogger().setLevel(logging.DEBUG)
+
+        self._button = QtWidgets.QPushButton(self)
+        self._button.setText('Test Me')    
+
+        layout = QtWidgets.QVBoxLayout()
+        # Add the new logging box widget to the layout
+        layout.addWidget(logTextBox.widget)
+        layout.addWidget(self._button)
+        self.setLayout(layout)    
+
+        # Connect signal to slot
+        self._button.clicked.connect(self.test)    
+
+    def test(self):
+        logging.debug('damn, a bug')
+        logging.info('something to remember')
+        logging.warning('that\'s not right')
+        logging.error('foobar')
+
+if (__name__ == '__main__'):
+    app = QtWidgets.QApplication(sys.argv)
+    dlg = MyDialog()
+    dlg.show()
+    sys.exit(app.exec_())
